2994456491e54367bc17824e6167a302257b6d6c
aledevv <alessandro.devidi@studenti.unitn.it>
2025-04-24 14:57:00 +0200

    setup part B

--- NLU/part_A/main.py
+++ NLU/part_A/main.py
@@ -3,7 +3,116 @@
 
 # Import everything from functions.py file
 from functions import *
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+    
+# ! Parameters
+hid_size = 200
+emb_size = 300
+
+lr = 0.0001 # learning rate
+clip = 5 # Clip the gradient
+
 
 if __name__ == "__main__":
-    #Wrtite the code to load the datasets and to run your functions
-    # Print the results
+    
+    # * Data preparation
+    tmp_train_raw, test_raw = load_ATIS()
+    train_raw, dev_raw, test_raw = create_dev_set(tmp_train_raw, test_raw)
+
+    words = sum([x['utterance'].split() for x in train_raw], []) # No set() since we want to compute 
+                                                            # the cutoff
+    corpus = train_raw + dev_raw + test_raw # We do not wat unk labels, 
+                                            # however this depends on the research purpose
+    slots = set(sum([line['slots'].split() for line in corpus],[]))
+    intents = set([line['intent'] for line in corpus])
+
+    lang = Lang(words, intents, slots, cutoff=0)
+    
+    # * Create datasets
+    train_dataset = IntentsAndSlots(train_raw, lang)
+    dev_dataset = IntentsAndSlots(dev_raw, lang)
+    test_dataset = IntentsAndSlots(test_raw, lang)
+    
+    # * Data loaders
+    train_loader = DataLoader(train_dataset, batch_size=128, collate_fn=collate_fn,  shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
+    
+    out_slot = len(lang.slot2id)
+    out_int = len(lang.intent2id)
+    vocab_len = len(lang.word2id)
+    
+    model = ModelIAS(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN).to(device)
+    model.apply(init_weights)
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+    criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
+    
+    
+    hid_size = 200
+    emb_size = 300
+
+    lr = 0.0001 # learning rate
+    clip = 5 # Clip the gradient
+
+
+    out_slot = len(lang.slot2id)
+    out_int = len(lang.intent2id)
+    vocab_len = len(lang.word2id)
+
+    n_epochs = 200
+    runs = 5
+
+    slot_f1s, intent_acc = [], []
+    for x in tqdm(range(0, runs)):
+        model = ModelIAS(hid_size, out_slot, out_int, emb_size, 
+                        vocab_len, pad_index=PAD_TOKEN).to(device)
+        model.apply(init_weights)
+
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+        criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+        criterion_intents = nn.CrossEntropyLoss()
+        
+
+        patience = 3
+        losses_train = []
+        losses_dev = []
+        sampled_epochs = []
+        best_f1 = 0
+        for x in range(1,n_epochs):
+            loss = train_loop(train_loader, optimizer, criterion_slots, 
+                            criterion_intents, model)
+            if x % 5 == 0:
+                sampled_epochs.append(x)
+                losses_train.append(np.asarray(loss).mean())
+                results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                            criterion_intents, model, lang)
+                losses_dev.append(np.asarray(loss_dev).mean())
+                f1 = results_dev['total']['f']
+
+                if f1 > best_f1:
+                    best_f1 = f1
+                else:
+                    patience -= 1
+                if patience <= 0: # Early stopping with patient
+                    break # Not nice but it keeps the code clean
+
+        results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
+                                                criterion_intents, model, lang)
+        intent_acc.append(intent_test['accuracy'])
+        slot_f1s.append(results_test['total']['f'])
+    slot_f1s = np.asarray(slot_f1s)
+    intent_acc = np.asarray(intent_acc)
+    print('Slot F1', round(slot_f1s.mean(),3), '+-', round(slot_f1s.std(),3))
+    print('Intent Acc', round(intent_acc.mean(), 3), '+-', round(slot_f1s.std(), 3))
+    
+    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
+    plt.title('Train and Dev Losses')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.plot(sampled_epochs, losses_train, label='Train loss')
+    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
+    plt.legend()
+    plt.show()
--- NLU/part_A/utils.py
+++ NLU/part_A/utils.py
@@ -1,1 +1,185 @@
 # Add functions or classes used for data loading and preprocessing
+import os
+import json
+from pprint import pprint
+import random
+import numpy as np
+import torch.utils.data as data
+from sklearn.model_selection import train_test_split
+from collections import Counter
+
+# ! GLOBAL VARIABLES
+device = 'cuda:0'
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1" # Used to report errors on CUDA side
+PAD_TOKEN = 0
+
+class Lang():
+    def __init__(self, words, intents, slots, cutoff=0):
+        self.word2id = self.w2id(words, cutoff=cutoff, unk=True) # convert words into idices
+        self.slot2id = self.lab2id(slots)   # convert labels (fromloc.city_name, toloc.city_name, ecc.) to ids (indices)
+        self.intent2id = self.lab2id(intents, pad=False) # intent is just a label so padding is not useful
+        self.id2word = {v:k for k, v in self.word2id.items()}   # these 3 functions are the inverse of the previous ones (useful for debugging)
+        self.id2slot = {v:k for k, v in self.slot2id.items()}
+        self.id2intent = {v:k for k, v in self.intent2id.items()}
+        
+    def w2id(self, elements, cutoff=None, unk=True):
+        vocab = {'pad': PAD_TOKEN}      # add padding (to fill phrases of different length and make them equally long)
+        if unk:
+            vocab['unk'] = len(vocab)   # add toke for unknown words (id = leng of vocab -> first available index, NOTE that you are increasing the vocab so every time you get a different id)
+        count = Counter(elements)   # get frequency of words
+        for k, v in count.items():            # k = word, v = frequency (e.g. "Tony": 3 -> the word "Tony" has 3 occurences)
+            if v > cutoff:  # * we consider in the vocabulary just the words having a frequency over the cutoff
+                vocab[k] = len(vocab)
+        return vocab
+    
+    def lab2id(self, elements, pad=True):   # get all the labels (slot and intent)
+        vocab = {}
+        if pad:
+            vocab['pad'] = PAD_TOKEN
+        for elem in elements:
+                vocab[elem] = len(vocab)    # also here assign the first available id
+        return vocab
+
+
+class IntentsAndSlots (data.Dataset):
+    # Mandatory methods are __init__, __len__ and __getitem__
+    def __init__(self, dataset, lang, unk='unk'):
+        self.utterances = []
+        self.intents = []
+        self.slots = []
+        self.unk = unk
+        
+        for x in dataset:
+            self.utterances.append(x['utterance'])
+            self.slots.append(x['slots'])
+            self.intents.append(x['intent'])
+
+        self.utt_ids = self.mapping_seq(self.utterances, lang.word2id)
+        self.slot_ids = self.mapping_seq(self.slots, lang.slot2id)
+        self.intent_ids = self.mapping_lab(self.intents, lang.intent2id)
+
+    def __len__(self):
+        return len(self.utterances)
+
+    def __getitem__(self, idx):
+        utt = torch.Tensor(self.utt_ids[idx])
+        slots = torch.Tensor(self.slot_ids[idx])
+        intent = self.intent_ids[idx]
+        sample = {'utterance': utt, 'slots': slots, 'intent': intent}
+        return sample
+    
+    # Auxiliary methods
+    
+    def mapping_lab(self, data, mapper):
+        return [mapper[x] if x in mapper else mapper[self.unk] for x in data]
+    
+    def mapping_seq(self, data, mapper): # Map sequences to number
+        res = []
+        for seq in data:
+            tmp_seq = []
+            for x in seq.split():
+                if x in mapper:
+                    tmp_seq.append(mapper[x])       # if it is in the mapping, assign the id
+                else:
+                    tmp_seq.append(mapper[self.unk])    # otherwise set to unknown
+            res.append(tmp_seq)
+        return res
+
+
+
+def load_ATIS():
+    def load_data(path):
+        dataset = []
+        with open(path) as f:
+            dataset = json.loads(f.read())
+        return dataset
+
+    tmp_train_raw = load_data(os.path.join('..','ATIS','train.json'))
+    test_raw = load_data(os.path.join('..','ATIS','test.json'))
+    # print('Train samples:', len(tmp_train_raw))
+    # print('Test samples:', len(test_raw))
+
+    # pprint(tmp_train_raw[0])
+    return tmp_train_raw, test_raw
+    
+def create_dev_set(tmp_train_raw, test_raw):
+    portion = 0.10  # use 10% of training set
+    
+    intents = [x['intent'] for x in tmp_train_raw] # We stratify on intents
+    count_y = Counter(intents)
+
+    labels = []
+    inputs = []
+    mini_train = []
+    
+    for id_y, y in enumerate(intents):
+        if count_y[y] > 1: # If some intents occurs only once, we put them in training
+            inputs.append(tmp_train_raw[id_y])
+            labels.append(y)
+        else:
+            mini_train.append(tmp_train_raw[id_y])
+    # Random Stratify
+    X_train, X_dev, y_train, y_dev = train_test_split(inputs, labels, test_size=portion, 
+                                                        random_state=42, 
+                                                        shuffle=True,
+                                                        stratify=labels)
+    X_train.extend(mini_train)
+    train_raw = X_train
+    dev_raw = X_dev
+
+    y_test = [x['intent'] for x in test_raw]
+
+    # Intent distributions
+    print('Train:')
+    pprint({k:round(v/len(y_train),3)*100 for k, v in sorted(Counter(y_train).items())})
+    print('Dev:'), 
+    pprint({k:round(v/len(y_dev),3)*100 for k, v in sorted(Counter(y_dev).items())})
+    print('Test:') 
+    pprint({k:round(v/len(y_test),3)*100 for k, v in sorted(Counter(y_test).items())})
+    print('='*89)
+    # Dataset size
+    print('TRAIN size:', len(train_raw))
+    print('DEV size:', len(dev_raw))
+    print('TEST size:', len(test_raw))
+    
+    return train_raw, dev_raw, test_raw
+
+def collate_fn(data):
+    def merge(sequences):
+        '''
+        merge from batch * sent_len to batch * max_len 
+        '''
+        lengths = [len(seq) for seq in sequences]
+        max_len = 1 if max(lengths)==0 else max(lengths)
+        # Pad token is zero in our case
+        # So we create a matrix full of PAD_TOKEN (i.e. 0) with the shape 
+        # batch_size X maximum length of a sequence
+        padded_seqs = torch.LongTensor(len(sequences),max_len).fill_(PAD_TOKEN)
+        for i, seq in enumerate(sequences):
+            end = lengths[i]
+            padded_seqs[i, :end] = seq # We copy each sequence into the matrix
+        # print(padded_seqs)
+        padded_seqs = padded_seqs.detach()  # We remove these tensors from the computational graph
+        return padded_seqs, lengths
+    # Sort data by seq lengths
+    data.sort(key=lambda x: len(x['utterance']), reverse=True) 
+    new_item = {}
+    for key in data[0].keys():
+        new_item[key] = [d[key] for d in data]
+        
+    # We just need one length for packed pad seq, since len(utt) == len(slots)
+    src_utt, _ = merge(new_item['utterance'])
+    y_slots, y_lengths = merge(new_item["slots"])
+    intent = torch.LongTensor(new_item["intent"])
+    
+    src_utt = src_utt.to(device) # We load the Tensor on our selected device
+    y_slots = y_slots.to(device)
+    intent = intent.to(device)
+    y_lengths = torch.LongTensor(y_lengths).to(device)
+    
+    new_item["utterances"] = src_utt
+    new_item["intents"] = intent
+    new_item["y_slots"] = y_slots
+    new_item["slots_len"] = y_lengths
+    return new_item
+
